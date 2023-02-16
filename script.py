9bd0e94f9e36385aa761d38cf7aaeaeb62c66cca
Melyn Atieno Were <melynatieno@gmail.com>
2023-02-17 01:18:53 +0300

    rename home html file to index html

--- script.py
+++ script.py
@@ -3,3 +3,6 @@ from jinja2 import Environment, FileSystemLoader;
 from json import load;
 
 template_env = Environment(loader=FileSystemLoader(searchpath="./"))
+template = template_env.get_template("layout.html")
+
+#with open("index.html") as output_file:
