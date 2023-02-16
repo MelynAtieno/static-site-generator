a05c46c25015bd76b09f528f34b391c48b72e670
Melyn Atieno Were <melynatieno@gmail.com>
2023-02-17 01:42:11 +0300

    render contents on the markdown file

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
-from markdown2 import markdown;
-from jinja2 import Environment, FileSystemLoader;
-from json import load;
+from markdown2 import markdown
+from jinja2 import Environment, FileSystemLoader
+from json import load
 
 template_env = Environment(loader=FileSystemLoader(searchpath="./"))
 template = template_env.get_template("layout.html")
@@ -8,7 +8,7 @@ template = template_env.get_template("layout.html")
 with open("website.md") as markdown_file:
     website = markdown(markdown_file.read())
 
-with open("index.html", "w") as output_file:
+with open('index.html', 'w') as output_file:
     output_file.write(
         template.render(
             website=website
